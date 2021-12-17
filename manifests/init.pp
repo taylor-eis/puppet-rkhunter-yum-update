33c17eefd3ed8f02a909caf28abae4b49e82788f
Brent Gerig <brgerig@taylor.edu>
2021-12-17 11:19:21 -0500

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+class rkhunter-yum-update {
+  case $facts['os']['release']['major'] {
+    '7': {
+      $pkg = 'yum-plugin-post-transaction-actions'
+    }
+    '8': {
+      $pkg = 'python3-dnf-plugin-post-transaction-actions'
+    }
+  }
+
+  $job = "#!/bin/bash
+
+if [[ -f /var/lib/rkhunter/updated.txt ]] ; then
+    while read in; do /usr/bin/rkhunter --propupdate "$in" > /dev/null; done < /var/lib/rkhunter/updated.txt
+    rm -rf /var/lib/rkhunter/updated.txt
+fi"
+
+  package { 'post-transactions':
+    ensure => latest,
+    name => $pkgq,
+  }
+
+  file { '/etc/yum/post-actions/rkhunter.action':
+    ensure => file,
+    content => "*:any:echo $name >> /var/lib/rkhunter/updated.txt",
+    require => Package['post-transactions'],
+  }
+
+  cron::daily { '0rkhunter':
+    command => $job,
+    user => root,
+  }
+}    
